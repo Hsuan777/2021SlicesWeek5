9e2d7b6843d6c88fdbe97d00d3138d8fb3739df3
Vic <Vic@gmail.com>
2021-08-02 16:13:56 +0800

    [ add ] 回覆效果

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,11 @@
-console.log('Hello!');
-
 $(document).ready(() => {
-  console.log('HesSchool Hello!');
+  $(".js-reply").click(()=> {
+    $(".reply__content").addClass("active");
+    $(".js-replyGroup").addClass("d-none");
+  });
+  
+  $(".js-cancel").click(()=> {
+    $(".reply__content").removeClass("active");
+    $(".js-replyGroup").removeClass("d-none");
+  });
 });
