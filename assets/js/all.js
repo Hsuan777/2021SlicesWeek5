8cf5f5a436c3c651958c470b5587c0319864d5d7
Vic <Vic@gmail.com>
2021-08-03 17:25:19 +0800

    Update 2021-08-03T09:25:14.037Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,34 @@
 "use strict";
 
-console.log('Hello!');
 $(document).ready(function () {
-  console.log('HesSchool Hello!');
+  $(".js-reply").click(function () {
+    $(".reply__content").addClass("active");
+    $(".js-replyGroup").addClass("d-none");
+  });
+  $(".js-cancel").click(function () {
+    $(".reply__content").removeClass("active");
+    $(".js-replyGroup").removeClass("d-none");
+  });
+  $(".js-viewInfo__more").click(function () {
+    if ($(".js-viewInfo__more").text() === "expand_more") {
+      $(".js-viewInfo__more").text('expand_less');
+    } else {
+      $(".js-viewInfo__more").text('expand_more');
+    }
+
+    $(".viewInfo").toggleClass("d-none");
+    $(".viewInfo__btns").toggleClass("active");
+  });
+  var htmlTitle = $('title');
+
+  switch (htmlTitle.text()) {
+    case "Assignment":
+      $('a[href="index.html"]').addClass('active');
+      break;
+
+    case "Admin":
+      $('a[href="admin.html"]').addClass('active');
+      break;
+  }
 });
 //# sourceMappingURL=all.js.map
