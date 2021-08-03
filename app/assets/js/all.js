9a09be32cf263c40e6797d82ca9eefaac4660911
Vic <Vic@gmail.com>
2021-08-03 15:55:10 +0800

    [ add ] edit modal

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -8,4 +8,15 @@ $(document).ready(() => {
     $(".reply__content").removeClass("active");
     $(".js-replyGroup").removeClass("d-none");
   });
+
+  $(".js-viewInfo__more").click(()=> {
+    if ($(".js-viewInfo__more").text() === "expand_more") {
+      $(".js-viewInfo__more").text('expand_less');
+    } else {
+      $(".js-viewInfo__more").text('expand_more');
+    }
+    console.log($(".js-viewInfo__more").text())
+    $(".viewInfo").toggleClass("d-none");
+    $(".viewInfo__btns").toggleClass("active");
+  });
 });
